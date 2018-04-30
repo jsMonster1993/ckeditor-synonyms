f99c7a7b99f7e97003789f920ebd8434a20b5975
jsMonster1993 <me@vikramgopali.com>
2018-04-30 22:14:07 +0530

    final commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,42 +10,19 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', ['$scope', function($scope) {
-    /*$scope.config = {};
-    $scope.config.toolbarGroups = [
-        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-        { name: 'forms', groups: [ 'forms' ] },
-        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-        { name: 'links', groups: [ 'links' ] },
-        { name: 'insert', groups: [ 'insert' ] },
-        { name: 'styles', groups: [ 'styles' ] },
-        { name: 'colors', groups: [ 'colors' ] },
-        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-        { name: 'tools', groups: [ 'tools' ] },
-        { name: 'others', groups: [ 'others' ] },
-        { name: 'about', groups: [ 'about' ] }
-    ];*/
-    CKEDITOR.plugins.add( 'timestamp', {
-        icons: 'timestamp',
-        init: function( editor ) {
-            //Plugin logic goes here.
-        }
-    });
+    $scope.synRes = [];
 
-    /*CKEDITOR.addCommand( 'insertTimestamp', {
-        exec: function( editor ) {
-            var now = new Date();
-            editor.insertHtml( 'The current date and time is: <em>' + now.toString() + '</em>' );
-        }
-    });
 
-    CKEDITOR.ui.addButton( 'Timestamp', {
-        label: 'Insert Timestamp',
-        command: 'insertTimestamp',
-        toolbar: 'insert'
-    });*/
+    $scope.selectOpt = (index,word)=>{
+        $scope.activeInd = index;
+        $scope.word = word;
+    };
 
+    $scope.addSynToEditor = ()=>{
+        $scope.htmlEditor = $scope.htmlEditor.split("<p>").join("").split("</p>").join(" ")+$scope.word;
+            $scope.word = "";
+        $scope.activeInd = "";
+    };
 
     $scope.htmlEditor = 'start typing ...';
 }]);
